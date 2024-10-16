9c6ff44c6038edabada664f1035c38e39d726353
Pavel <120335607+Gribok90@users.noreply.github.com>
2024-10-17 01:55:55 +0400

    сценарий для метода post_v1_account_login с новыми кредами

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -4,14 +4,16 @@ from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 
-
+#4. сценарий для метода post_v1_account_login - Authenticate via credentials
 def test_post_v1_account():
-    # Регистрация пользователя
+
+    # Объявляем креды
+
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'pt18'
+    login = 'pt21'
     password = '123456789'
     email = f'{login}@mail.com'
     json_data = {
@@ -20,10 +22,25 @@ def test_post_v1_account():
         'password': password,
     }
 
+    # Проверить что не созданный пользователь не может войти
+    response = login_api.post_v1_account_login(json_data=json_data)
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 400, "Не зарегистрированный пользователь  смог авторизоваться"
+
+    # Регистрация нового пользователя
     response = account_api.post_v1_account(json_data=json_data)
     print(response.status_code)
     print(response.text)
     assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
+
+    # Проверить, что пользователь не может залогиниться без активации
+    response = login_api.post_v1_account_login(json_data=json_data)
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 403, "Пользователь смог авторизоваться без активации!"
+
+
     # Получить письма из почтового сервера
     response = mailhog_api.get_api_v2_messages()
     print(response.status_code)
@@ -60,9 +77,3 @@ def get_activation_token_by_login(login, response):
         if user_login == login:
             token = user_data['ConfirmationLinkUrl'].split('/')[-1]
     return token
-
-
-
-
-
-
