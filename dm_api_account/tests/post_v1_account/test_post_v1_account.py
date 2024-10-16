8fbfc4bcb4d771787bc666d1e6e037a65c378084
Pavel <120335607+Gribok90@users.noreply.github.com>
2024-10-16 23:08:47 +0400

    фикс job имени и веток

--- dm_api_account/tests/post_v1_account/test_post_v1_account.py
+++ dm_api_account/tests/post_v1_account/test_post_v1_account.py
@@ -11,7 +11,7 @@ def test_post_v1_account():
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'pt6'
+    login = 'pt10'
     password = '123456789'
     email = f'{login}@mail.com'
     json_data = {
