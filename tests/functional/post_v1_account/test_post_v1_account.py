300a95ab59bf08b327c80c2e6b444443785bbba4
Pavel <120335607+Gribok90@users.noreply.github.com>
2024-10-17 23:28:05 +0400

    добавили логгирующий клиент

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -3,6 +3,17 @@ from json import loads
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
+import structlog
+
+structlog.configure(
+    processors={
+        structlog.processors.JSONRenderer(
+            indent=6,
+            ensure_ascii=True,
+            sort_keys=True
+        )
+    }
+)
 
 
 def test_post_v1_account():
@@ -11,7 +22,7 @@ def test_post_v1_account():
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'pt50'
+    login = 'pt61'
     password = '123456789'
     email = f'{login}@mail.com'
     json_data = {
@@ -21,13 +32,10 @@ def test_post_v1_account():
     }
 
     response = account_api.post_v1_account(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
+
     assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
     # Получить письма из почтового сервера
     response = mailhog_api.get_api_v2_messages()
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Письма не были получены"
 
     # Получить активационный токен
@@ -39,22 +47,18 @@ def test_post_v1_account():
     # Активация пользователя
     response = account_api.put_v1_account_token(token=token)
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Пользователь не был активирован"
 
     # Авторизоваться
 
     response = login_api.post_v1_account_login(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Пользователь не смог авторизоваться"
 
 
 def get_activation_token_by_login(
         login,
         response
-        ):
+):
     token = None
     for item in response.json()['items']:
         user_data = loads(item['Content']['Body'])
