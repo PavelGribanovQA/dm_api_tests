fb1be1264f29e8c105024d4fe740e6969dbdd94f
Pavel <120335607+Gribok90@users.noreply.github.com>
2024-11-07 13:08:34 +0400

    добавил проверки PyHamcrest в позитивный тест метода get_v1_account

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -126,7 +126,8 @@ class AccountHelper:
             login: str,
             password: str,
             remember_me: bool = True,
-            validate_response=False
+            validate_response=False,
+            validate_headers=False
     ):
         login_credentials = LoginCredentials(
             login=login,
@@ -137,7 +138,7 @@ class AccountHelper:
             login_credentials=login_credentials,
             validate_response=validate_response
         )
-        if response.status_code == 200:
+        if validate_headers:
             assert response.headers["x-dm-auth-token"], "Токен для пользователя не был получен"
         return response
 
